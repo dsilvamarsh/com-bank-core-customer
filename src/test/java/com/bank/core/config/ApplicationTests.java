10430d6db761593b75c370ac0ab2a5215921b6e2
Marsh <dsilvamarsh@gmail.com>
2023-05-12 00:24:03 +0100

    modifying launcher names

--- src/test/java/com/bank/core/config/ApplicationTests.java
+++ src/test/java/com/bank/core/config/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ComBankCoreCustomerApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
