946cfa7325fe6d0b058072133cf1f519a7550cec
Marsh <dsilvamarsh@gmail.com>
2024-02-23 13:36:22 +0000

    fix for lomboc

--- src/main/java/com/bank/core/controller/CustomerController.java
+++ src/main/java/com/bank/core/controller/CustomerController.java
@@ -1,7 +1,6 @@
 package com.bank.core.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +19,7 @@ public class CustomerController {
 	private CustomerRepository repo;
 	@GetMapping("/customer/{id}")
 	public Customer find(@PathVariable int id) {
-		log.debug("Inside customer controller");
+		//log.debug("Inside customer controller");
 	
 		return repo.findById(id).get();
 	}
